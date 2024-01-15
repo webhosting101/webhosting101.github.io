9ec75d1c3a9b3099875799f7694e9b875f068492
Nomad <ljtwebhosting@gmail.com>
2024-01-15 20:07:26 +0800

    logic for food flatfile

--- testPDF/src/foodUtil.js
+++ testPDF/src/foodUtil.js
@@ -5,6 +5,20 @@ generateMeals = () => {
         contents = loadFile();
     }
     console.log(contents);
+
+    var eachCSVLine = contents.split('\n');
+    var csvtoJSONFile = [];
+    var headers = eachCSVLine[0].split(',');
+    for(var i = 1; i < arr.length; i++) {
+        var data = arr[i].split(',');
+        var obj = {};
+        for(var j = 0; j < data.length; j++) {
+            obj[headers[j].trim()] = data[j].trim();
+        }
+        csvtoJSONFile.push(obj);
+    }
+
+    console.log(csvtoJSONFile);
 }
 loadFile = () => {
     var result = null;
